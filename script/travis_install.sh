ccf14cf2d463f8400dab35497080b575a3e75ac1
offa <bm-dev@yandex.com>
2016-08-29 19:15:29 +0200

    Clang CI builds fixed.

---
+++ script/travis_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+## Install GTest
+export GTEST_VERSION=master
+export GTEST=googletest-${GTEST_VERSION}
+
+wget https://github.com/google/googletest/archive/${GTEST_VERSION}.tar.gz
+tar -xzvf *.tar.gz
+cd ${GTEST}
+mkdir build && cd build
+
+if [[ "${CXX}" = clang* ]] ; then
+    cmake -DCMAKE_CXX_FLAGS="-stdlib=libc++" ..
+else
+    cmake ..
+fi
+
+make -j4 && sudo make install && cd ../..
+
