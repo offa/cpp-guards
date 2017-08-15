be3595de04d68d638b05a38716a5101186206c67
offa <bm-dev@yandex.com>
2017-08-15 19:39:37 +0200

    Removing the tar verbose flag.

--- script/travis_install.sh
+++ script/travis_install.sh
@@ -7,7 +7,7 @@ export GTEST_VERSION=master
 export GTEST=googletest-${GTEST_VERSION}
 
 wget https://github.com/google/googletest/archive/${GTEST_VERSION}.tar.gz
-tar -xzvf *.tar.gz
+tar -xzf *.tar.gz
 cd ${GTEST}
 mkdir build && cd build
 
